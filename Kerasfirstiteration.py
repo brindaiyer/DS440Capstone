a6bb82781ffb3b320633fa3e1debaeb117f384f1
brindaiyer <32623981+brindaiyer@users.noreply.github.com>
2022-06-17 13:54:11 -0400

    Update Kerasfirstiteration.py

--- Kerasfirstiteration.py
+++ Kerasfirstiteration.py
@@ -8,6 +8,7 @@ import math
 import random
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pickle
 from pylab import *
 from sklearn.manifold import TSNE
 from keras.layers import Input
@@ -17,25 +18,10 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.utils import class_weight
 
 
-#reading in and pre-processing data
-columns = ['txId', 'timestep']
-class_names = ['Illicit', 'Legal']
-
-# Name the columns without known names
-for x in range(165) :
-    columns.append('col'+ str(x))
-
-
-classes_csv = pd.read_csv('C:/Users/brind/Downloads/elliptic_txs_classes.csv/elliptic_txs_classes.csv')
-edgelist_csv = pd.read_csv('C:/Users/brind/Downloads/elliptic_txs_edgelist.csv/elliptic_txs_edgelist.csv')
-features_csv = pd.read_csv('C:/Users/brind/Downloads/elliptic_txs_features.csv/elliptic_txs_features.csv',names=columns)
-
-#flattening data, appending class to features
-data = features_csv.assign(result=classes_csv['class'])
+data = pd.read_csv('C:/Users/brind/Documents/startingdata.csv')
 
 # Trim the data to include only labeled data.
 old_data = data[data['result'] == "unknown"]
-test_old_data = old_data.pop('result')
 new_data = data[data['result'] != "unknown"]
 new_data['result'] = pd.to_numeric(new_data.result) - 1
 
@@ -43,19 +29,21 @@ new_data['result'] = pd.to_numeric(new_data.result) - 1
 msk = np.random.rand(len(new_data)) < 0.8
 
 x_train = new_data[msk]
-#x_train.pop('txId')
+x_train.pop('txId')
 
 x_test = new_data[~msk]
-#x_test.pop('txId')
+predictionid = x_test.pop('txId')
+print(predictionid)
 
-#y_train = x_train.pop('result')
 y_test = x_test.pop('result')
 
-fraud = x_train[x_train['result'] == 1]#majority class
-nonfraud = x_train[x_train['result'] == 0]#minority class
-nonfraud = nonfraud.sample(len(fraud),replace=True)
-
-x_train = pd.concat([nonfraud,fraud], axis=0)
+##fraud = x_train[x_train['result'] == 1]#majority class
+##nonfraud = x_train[x_train['result'] == 0]#minority class
+##nonfraud = nonfraud.sample(len(fraud),replace=True)
+####pd.concat
+####
+##
+##x_train = pd.concat([nonfraud,fraud], axis=0)
 y_train = x_train.pop('result')
 
 
@@ -75,10 +63,12 @@ model.compile(optimizer='adam',
 
 ####
 ##### train the model
-model.fit(x_train, y_train, epochs = 1) #epochs stands for how many times you go through training set
+#model.fit(x_train, y_train, epochs = 5) #epochs stands for how many times you go through training set
+
+#saved_model = pickle.dumps(model)
 ####
 ### test the model
-test_loss, test_acc = model.evaluate(x_test, y_test, verbose=0)
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose = 2)
 print('\nTest accuracy:', test_acc)
 ##
 ### Make predictions
@@ -99,28 +89,29 @@ TP = 0
 TN = 0
 predicted = []
 
-pred = np.argmax(predictions,axis=1)
 for i in range(len(y_test)):
-    if pred[i] == 0 and y_test.iloc[i] == 1:
+    pred = np.argmax(predictions[i])
+    predicted.append(pred)
+    if pred == 0 and y_test.iloc[i] == 1:
         FN += 1
     
-    if pred[i] == 1 and y_test.iloc[i] == 0:
+    if pred == 1 and y_test.iloc[i] == 0:
         FP += 1
 
-    if pred[i] == 0 and y_test.iloc[i] == 0:
+    if pred == 0 and y_test.iloc[i] == 0:
         TN += 1
 
-    if pred[i] == 1 and y_test.iloc[i] == 1:
+    if pred == 1 and y_test.iloc[i] == 1:
         TP += 1
         
-    if pred[i] != y_test.iloc[i]:
+    if pred != y_test.iloc[i]:
         incorrect +=1
 
 submission = pd.DataFrame({
-        "txId": [x_test.iloc[i,0] for i in range(len(x_test))],#x_test["timestep"],
+        "txId": [predictionid.iloc[i] for i in range(len(predictionid))],#x_test["timestep"],
         "Class": pred
     })
-print(submission)
+
 ##
 ##submission.to_csv(r'C:\Users\brind\Documents\DS440\EvolveGCN\data\elliptic_bitcoin_dataset\KerasSequentialModels.csv', index = None, header=True)
 
@@ -146,4 +137,3 @@ print(confusion_matrix)
 
 sns.heatmap(confusion_matrix, annot=True)
 plt.show()
-
